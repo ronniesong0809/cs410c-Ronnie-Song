534a71445fa0124204c045f9f9dd97c188e84fe1
Ronnie Song <qihao@augusta.cs.pdx.edu>
2018-10-16 15:30:16 -0700

    hw2

---
+++ hw2/app.py
@@ -0,0 +1,11 @@
+import flask
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'welcome to my site'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888,debug=True)
+
+
