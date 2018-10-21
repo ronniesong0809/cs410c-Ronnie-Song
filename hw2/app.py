db73272e4fc0ca4272eddcd25e6b61de05bf6bad
ronsong <ronsong@pdx.edu>
2018-10-21 15:00:23 -0700

    app.py, add.py and index.py

--- hw2/app.py
+++ hw2/app.py
@@ -1,11 +1,22 @@
+"""
+A simple guestbook flask app.
+"""
 import flask
+from index import Index
+from add import Add
+
+
 app = flask.Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'welcome to my site'
+app.add_url_rule('/',
+                 view_func=Index.as_view('index'),
+                 methods=["GET"])
+
+app.add_url_rule('/add/',
+                 view_func=Add.as_view('add'),
+                 methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888,debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
 
 
