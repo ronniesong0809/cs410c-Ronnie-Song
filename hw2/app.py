5b09020b7f7fb2e89b2045456dc451b88c965fa0
ronsong <ronsong@pdx.edu>
2018-10-21 16:54:12 -0700

    version that derive from v2

--- hw2/app.py
+++ hw2/app.py
@@ -1,7 +1,8 @@
 """
-A simple guestbook flask app.
+A recipes flask app.
 """
 import flask
+from flask.views import MethodView
 from index import Index
 from add import Add
 
@@ -17,6 +18,6 @@ app.add_url_rule('/add/',
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
 
--- hw2/gbmodel/model_sqlite3.py
+++ hw2/gbmodel/model_sqlite3.py
@@ -1,9 +1,33 @@
+"""
+A recipes flask webapp that derive from guestbook.
+ata is stored in a SQLite database that looks something like the following:
+
++-------+----------+---------------------------+---------+--------------+-----------------------------------------------------------+
+| Title | Author   | Ingredient                | Time    | Skill Level  | Description                                               |
++=======+==========+===========================+=========+==============+===========================================================+
+| Steak | John Doe | Round Steak, Salt, Pepper | 10 mins | Intermediate | Cook for 1 minute each side in a large cast iron skullet. |
++-------+----------+---------------------------+---------+--------------+-----------------------------------------------------------+
+
+This can be created with the following SQL (see bottom of this file):
+
+    create table recipes (name text, email text, signed_on date, message);
+
+"""
+from datetime import date
 from .Model import Model
+import sqlite3
+DB_FILE = 'entries.db'    # file for our Database
 
 class model(Model):
     def __init__(self):
         # Make sure our database exists
-        pass
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("select count(rowid) from guestbook")
+        except sqlite3.OperationalError:
+            cursor.execute("create table guestbook (name text, email text, signed_on date, message)")
+        cursor.close()
 
     def select(self):
         """
@@ -11,7 +35,10 @@ class model(Model):
         Each row contains: name, email, date, message
         :return: List of lists containing all rows of database
         """
-        pass
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM guestbook")
+        return cursor.fetchall()
 
     def insert(self, name, email, message):
         """
@@ -22,4 +49,11 @@ class model(Model):
         :return: True
         :raises: Database errors on connection and insertion
         """
-        pass
+        params = {'name':name, 'email':email, 'date':date.today(), 'message':message}
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("insert into guestbook (name, email, signed_on, message) VALUES (:name, :email, :date, :message)", params)
+
+        connection.commit()
+        cursor.close()
+        return True
