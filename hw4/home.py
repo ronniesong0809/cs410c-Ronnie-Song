b1f69106407837f2280f7c01ad7649199551ab3b
ronsong <ronsong@pdx.edu>
2018-11-02 05:37:51 -0700

    derived: derived from hw3

---
+++ hw4/home.py
@@ -0,0 +1,10 @@
+"""
+the landing page for recipe app.
+"""
+from flask import render_template
+from flask.views import MethodView
+import gbmodel
+
+class Home(MethodView):
+    def get(self):
+        return render_template('home.html')
