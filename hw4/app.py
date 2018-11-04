5b9b48fa227b8ed44c00fcc91af1412d564b2f1d
ronsong <ronsong@pdx.edu>
2018-11-04 02:43:51 -0800

    fix: removed port:8000 from app.py

--- hw4/app.py
+++ hw4/app.py
@@ -38,6 +38,6 @@ app.add_url_rule('/remove/',
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
