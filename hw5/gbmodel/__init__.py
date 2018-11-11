c4e78c5597d5a6444f4f741d6e2ad3c2ee3d38ed
ron <ronniesong0809>
2018-11-11 01:55:38 -0800

    feat: add google cloud datastore

--- hw5/gbmodel/__init__.py
+++ hw5/gbmodel/__init__.py
@@ -1,7 +1,8 @@
 """ init model"""
 #model_backend = 'pydict'
 #model_backend = 'pylist'
-model_backend = 'sqlite3'
+#model_backend = 'sqlite3'
+model_backend = 'datastore'
 
 if model_backend == 'sqlite3':
     from .model_sqlite3 import model
@@ -9,6 +10,8 @@ elif model_backend == 'pylist':
     from .model_pylist import model
 elif model_backend == 'pydict':
     from .model_pydict import model
+elif model_backend == 'datastore':
+    from .model_datastore import model
 else:
     raise ValueError("No appropriate databackend configured. ")
 
